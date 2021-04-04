eb4ce1de4d9aca730ec5380f9f01482ffd39a624
Ben Reaves <rbreaves@gmail.com>
2021-04-04 00:12:37 -0500

    Added several apps, virtualbox, font, oh-my-zsh

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Fast Dev Environment Setup Script
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/rbreaves/kairos/master/linux.sh)"
 ```
 
+Parameter options
+
+-l (lowercase L) - Install sublimetext 3 only, normal prompts
+-k - automatically install Kinto without prompting
+-a - Install all options, as few prompts as possible
+
 ## Install (Windows)
 
 ```
--- linux.sh
+++ linux.sh
@@ -1,54 +1,126 @@
 #!/usr/bin/env bash
 
+echo -e "Kairos - Fast Dev Environment Setup Script\n"
+
+
+# Sets defaults and asks all questions before the installer begins
+
+apps="sublime-text xfce4-terminal vim zsh tmux remmina font-manager"
+
+if [ "$1" == "-a" ];then
+	kintoyn="y"
+	virtualboxyn="y"
+	apps="${apps} virtualbox virtualbox-ext-pack"
+	echo -e "All programs and options will be installed.\n"
+	echo "Kinto remapper will be installed."
+	echo -e "Virtualbox w/ extension pack will be installed.\n"
+	echo "These programs will be installed."
+	echo -e "$apps\n"
+	read -p 'Press Any key to confirm...' anykey
+else
+	kintoyn="n"
+	virtualboxyn="n"
+fi
+
+if [ $# -eq 0 ] || [ "$1" == "-l" ];then
+	echo -e "Will install all programs first, but first answer the following questions."
+	while true; do
+	read -rep $'\nWould you like to install or reinstall Kinto? [Y/n]\n' kintoyn
+		case $kintoyn in
+			[Yy]* ) echo "Will install Kinto";kintoyn="y"; break;;
+			[Nn]* ) echo "Installing Kinto will be skipped";kintoyn="n";break;;
+			* ) echo "Will install Kinto";kintoyn="y";break;;
+		esac
+	done
+
+	while true; do
+	read -rep $'\nWould you like to install Virtualbox? [Y/n]\n' virtualboxyn
+		case $kintoyn in
+			[Yy]* ) echo "Will install Virtualbox";virtualboxyn="y"; break;;
+			[Nn]* ) echo "Installing Virtualbox will be skipped";virtualboxyn="n";break;;
+			* ) echo "Will install Virtualbox";virtualboxyn="y";break;;
+		esac
+	done
+
+	echo ""
+fi
+
+if [ "$1" == "-k" ] || [ "$2" == "-k" ];then
+	echo "Will install Kinto"
+	kintoyn="y"
+elif [ "$1" == "-l" ] || [ "$2" == "-l" ];then
+	echo "Sublime only install"
+	apps="sublime-text"
+elif [ "$1" == "-lk" ] || [ "$2" == "-kl" ];then
+	echo "Will install Kinto"
+	kintoyn="y"
+	echo "Sublime only install"
+	apps="sublime-text"
+fi
+
+# Defaults end, questions are done.
+
+
+# Import distro and dename info
+
 typeset -l distro
 distro=$(awk -F= '$1=="NAME" { gsub("[\",!,_, ]","",$2);print $2 ;}' /etc/os-release)
 typeset -l dename
 dename=$(./dename.sh | cut -d " " -f1)
 
-echo "Install Distro specific programs..."
+# Running the script
+
+echo -e "Install Distro specific programs...\n"
 
 if pkgmgr="$( which apt-get )" 2> /dev/null; then
 	echo "Debain based setup"
-	sudo apt-get update
-
-	# Install Sublime Text 3
+	sudo $pkgmgr update
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	sudo apt-get -y install apt-transport-https
+	sudo $pkgmgr -y install apt-transport-https
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	sudo apt-get update
-	sudo apt-get install sublime-text
+	sudo apt-get install git $apps
 elif pkgmgr="$( which dnf )" 2> /dev/null; then
 	echo "Fedora based setup"
-	sudo dnf check-update
+	sudo $pkgmgr check-update
 	sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-	sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-	sudo dnf -y install sublime-text
+	sudo $pkgmgr config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+	sudo $pkgmgr -y install git $apps
 elif pkgmgr="$( which pacman )" 2> /dev/null; then
 	echo "Arch-based"
-	sudo pacman -Syy
+	sudo $pkgmgr -Syy
 	curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 	echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-	yes | sudo pacman -Syu sublime-text
+	yes | sudo $pkgmgr -Syu $apps
 elif pkgmgr="$( which yum )" 2> /dev/null; then
 	echo "Redhat-based"
 	sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 	sudo yum-config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-	sudo yum -y install sublime-text
+	sudo $pkgmgr -y install git $apps
+elif pkgmgr="$( which zypper )" 2> /dev/null; then
+	echo "openSUSE-based"
+	sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+	sudo $pkgmgr addrepo -g -f https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+	sudo $pkgmgr refresh
+	sudo $pkgmgr -y install git $apps
 else
 	echo "Package manager not found/supported" >&2
 	exit 1
 fi
 
-while true; do
-read -rep $'\nWould you like to install or reinstall Kinto? [Y/n]\n' yn
-	case $yn in
-		[Yy]* ) yn="y"; break;;
-		[Nn]* ) yn="n";break;;
-		* ) yn="y";break;;
-	esac
-done
+if [ "$apps" != "sublime-text" ];then
+	echo "Install files, fonts, etc..."
+	mkdir ~/.fonts
+	wget https://github.com/powerline/fonts/blob/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf -o ~/.fonts
+	fc-cache -f -v
+fi
 
-if [ "$yn" == "y" ];then
+if [ "$kintoyn" == "y" ];then
 	echo "Installing Kinto..."
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/rbreaves/kinto/master/install/linux.sh)"	
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/rbreaves/kinto/master/install/linux.sh)"
+fi
+
+if [ "$apps" != "sublime-text" ];then
+	echo "Install oh-my-zsh..."
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
