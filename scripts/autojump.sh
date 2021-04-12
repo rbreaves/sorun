522bc775a7ffb0dbf87bb48170aea52d27f2437a
Ben Reaves <rbreaves@gmail.com>
2021-04-12 01:57:39 -0500

    Disabling homebrew by default & changing autojump to source from debian/ubuntu repos

--- scripts/autojump.sh
+++ scripts/autojump.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
-if ! [ -f /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh ];then
-	brew install autojump >/dev/null 2>&1
-else
-	echo "Autojump is already installed. Skipping."
-fi
+# Brew based installed
 
-if ! grep -q 'profile.d/autojump' ~/.bashrc;then
-	echo "[ -f /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh ] && . /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh" >> ~/.bashrc
-fi
+# if ! [ -f /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh ];then
+# 	brew install autojump >/dev/null 2>&1
+# else
+# 	echo "Autojump is already installed. Skipping."
+# fi
 
-if ! grep -q 'profile.d/autojump' ~/.zshrc;then
-	echo "[ -f /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh ] && . /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh" >> ~/.zshrc
+# if ! grep -q 'profile.d/autojump' ~/.bashrc;then
+# 	echo "[ -f /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh ] && . /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh" >> ~/.bashrc
+# fi
+
+# if ! grep -q 'profile.d/autojump' ~/.zshrc;then
+# 	echo "[ -f /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh ] && . /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh" >> ~/.zshrc
+# fi
+
+# End Brew based insall
+
+if [ -f /usr/share/autojump/autojump.sh ];then
+	if ! grep -q 'autojump.sh' ~/.bashrc;then
+		echo "[ -f /usr/share/autojump/autojump.sh ] && . /usr/share/autojump/autojump.sh"  >> ~/.bashrc
+	fi
+
+	if ! grep -q 'autojump.sh' ~/.zshrc;then
+		echo "[ -f /usr/share/autojump/autojump.sh ] && . /usr/share/autojump/autojump.sh"  >> ~/.zshrc
+	fi
 fi
