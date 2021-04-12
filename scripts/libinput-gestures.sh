2a8a338859a8732b0fb093dd50841b57e4caad55
Ben Reaves <rbreaves@gmail.com>
2021-04-12 18:55:11 -0500

    Revised libinput guestures regex

--- scripts/libinput-gestures.sh
+++ scripts/libinput-gestures.sh
@@ -9,10 +9,15 @@ sudo make install
 
 cp /etc/libinput-gestures.conf ~/.config/libinput-gestures.conf
 
-sed -i "s/gesture swipe up   _internal ws_up/gesture swipe left 3   _internal ws_up/g" ~/.config/libinput-gestures.conf
-sed -i "s/gesture swipe down    _internal ws_down/gesture swipe right 3    _internal ws_down/g" ~/.config/libinput-gestures.conf
+sed -i "s/gesture swipe up  _internal ws_up/gesture swipe left 3    xdotool set_desktop --relative -- -1/g" ~/.config/libinput-gestures.conf
+sed -i "s/gesture swipe down	_internal ws_down/gesture swipe right 3    xdotool set_desktop --relative 1/g" ~/.config/libinput-gestures.conf
+
+libinput-gestures-setup start
+libinput-gestures-setup autostart
+libinput-gestures-setup status
 
 echo "May need to logoff and back on for 3 finger left and right gestures to work for virtual desktops."
 echo ""
 echo "Installing Gestures app gives a GUI interface for gestures. May be commented out in Kairos."
 echo "https://gitlab.com/cunidev/gestures"
+echo ""
