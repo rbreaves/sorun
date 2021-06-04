15ddb0cd720690bab4d2e47000fdd8eda3e7bae8
Ben Reaves <rbreaves@gmail.com>
2021-06-04 17:45:33 -0500

    Fixed shortcuts, added wallpapers

--- scripts/pop_os-defaults.sh
+++ scripts/pop_os-defaults.sh
@@ -18,7 +18,15 @@ main () {
 		fi
 	fi
 
-	gsettings get org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/pop/kate-hazen-pop-m3lvin.png'
+	width=$(xdpyinfo | awk '/dimensions/{sub(/x.*/,""); print $2}')
+	mkdir -p $HOME/Pictures/wallpapers
+	sudo cp -a ./assets/wallpapers/. $HOME/Pictures/wallpapers/
+	# Credit Any Holmes
+	# Unsplash license for reuse
+	# https://unsplash.com/photos/rCbdp8VCYhQ
+
+	# gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/pop/kate-hazen-pop-m3lvin.png'
+	gsettings set org.gnome.desktop.background picture-uri 'file://$HOME/Pictures/wallpapers/sorunme-1920x1200.jpg'
 
 	# Just Perfection
 	gnome-shell-extension-installer 3843
@@ -39,17 +47,21 @@ main () {
 
 	gnome-extensions disable ubuntu-appindicators@ubuntu.com
 
-	sudo cp ./assets/whiskermenu/128x128_kairos.png /usr/share/icons/hicolor/128x128/apps/128x128_kairos.png
-	sudo cp ./assets/whiskermenu/16x16_kairos.png /usr/share/icons/hicolor/16x16/apps/16x16_kairos.png
-	sudo cp ./assets/whiskermenu/22x22_kairos.png /usr/share/icons/hicolor/22x22/apps/22x22_kairos.png
-	sudo cp ./assets/whiskermenu/24x24_kairos.png /usr/share/icons/hicolor/24x24/apps/24x24_kairos.png
-	sudo cp ./assets/whiskermenu/256x256_kairos.png /usr/share/icons/hicolor/256x256/apps/256x256_kairos.png
-	sudo cp ./assets/whiskermenu/32x32_kairos.png /usr/share/icons/hicolor/32x32/apps/32x32_kairos.png
-	sudo cp ./assets/whiskermenu/48x48_kairos.png /usr/share/icons/hicolor/48x48/apps/48x48_kairos.png
-	sudo cp ./assets/whiskermenu/64x64_kairos.png /usr/share/icons/hicolor/64x64/apps/64x64_kairos.png
-	sudo cp ./assets/whiskermenu/scalable_kairos.svg /usr/share/icons/hicolor/scalable/apps/scalable_kairos.svg
+	sudo cp ./assets/icons/128x128/sorunme.png /usr/share/icons/hicolor/128x128/apps/sorunme.png
+	sudo cp ./assets/icons/16x16/sorunme.png /usr/share/icons/hicolor/16x16/apps/sorunme.png
+	sudo cp ./assets/icons/22x22/sorunme.png /usr/share/icons/hicolor/22x22/apps/sorunme.png
+	sudo cp ./assets/icons/24x24/sorunme.png /usr/share/icons/hicolor/24x24/apps/sorunme.png
+	sudo cp ./assets/icons/256x256/sorunme.png /usr/share/icons/hicolor/256x256/apps/sorunme.png
+	sudo cp ./assets/icons/32x32/sorunme.png /usr/share/icons/hicolor/32x32/apps/sorunme.png
+	sudo cp ./assets/icons/48x48/sorunme.png /usr/share/icons/hicolor/48x48/apps/sorunme.png
+	sudo cp ./assets/icons/64x64/sorunme.png /usr/share/icons/hicolor/64x64/apps/sorunme.png
+	sudo cp ./assets/icons/scalable/sorunme.svg /usr/share/icons/hicolor/scalable/apps/sorunme.svg
+	sudo cp ./assets/icons/scalable/sorunme.svg /usr/share/icons/McMojave-circle/apps/scalable/sorunme.png
 
 	cp ./assets/Pop_OS/gtk.css ~/.config/gtk-3.0/gtk.css
+	mkdir -p ~/.icons
+	cp -a ./assets/icons/. ~/.icons/
+	ln -s ~/.icons/scalable/sorunme.svg ~/.icons/sorunme.svg
 	cp -a ./assets/xfce4/. ~/.config/xfce4/
 	cp -a ./assets/plank/. ~/.config/plank/
 	cp -a ./assets/autostart_xfce4/. ~/.config/autostart/
